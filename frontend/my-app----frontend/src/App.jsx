8bbea73d316806414d4149ff9155837ce57704cc
XYZ <alifyousufabdullah@gmail.com>
2026-07-02 03:29:25 +0600

    frontend works

--- frontend/my-app----frontend/src/App.jsx
+++ frontend/my-app----frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+
 import WelcomePage from "./WelcomePage";
 import LoginPage from "./LoginPage";
+
+import "./index.css"; // or "./App.css" if you use that
 
 function App() {
   const [page, setPage] = useState("welcome");
