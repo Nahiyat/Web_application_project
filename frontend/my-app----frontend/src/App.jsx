aa5c0a1cc01aa4cc48719528839c974d376cc373
Alif <alifyousufabdullah@gmail.com>
2026-07-23 06:51:48 +0600

    Update App.jsx

--- frontend/my-app----frontend/src/App.jsx
+++ frontend/my-app----frontend/src/App.jsx
@@ -2,21 +2,48 @@ import { useState } from "react";
 
 import WelcomePage from "./WelcomePage";
 import LoginPage from "./LoginPage";
+import ChessBoard from "./ChessBoard";
 
 import "./index.css"; // or "./App.css" if you use that
 
 function App() {
-  const [page, setPage] = useState("welcome");
 
-  return (
-    <>
-      {page === "welcome" ? (
-        <WelcomePage onNext={() => setPage("login")} />
-      ) : (
-        <LoginPage />
-      )}
-    </>
-  );
+  const [page, setPage] = useState("welcome");
+
+
+
+  return (
+
+    <>
+
+      {page === "welcome" && (
+
+        <WelcomePage onNext={() => setPage("login")} />
+
+      )}
+
+
+
+      {page === "login" && (
+
+        <LoginPage onNext={() => setPage("chessboard")} />
+
+      )}
+
+
+
+      {page === "chessboard" && (
+
+        <ChessBoard />
+
+      )}
+
+    </>
+
+  );
+
 }
+
+
 
 export default App;
