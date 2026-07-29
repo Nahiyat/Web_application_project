3be6350a4977510b46d517e7af8f5ea6a33b370e
Nahiyat <nahiyat20003@gmail.com>
2026-07-30 02:58:55 +0600

    Fixed issue with db.Replaced postgresSQL with SQLite. fixed requirement.txt

--- frontend/my-app----frontend/src/main.jsx
+++ frontend/my-app----frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import RegisterPage from "./pages/RegisterPage.jsx";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RegisterPage />
   </React.StrictMode>
 );
