cf48569a933c94a58094a05f37ba2f69fde8df0f
Alif <alifyousufabdullah@gmail.com>
2026-08-03 20:05:36 +0600

    Update LoginPage.jsx

--- frontend/my-app----frontend/src/LoginPage.jsx
+++ frontend/my-app----frontend/src/LoginPage.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
 
-function LoginPage() {
+function LoginPage({ onNext, setUser }) {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Save logged-in user
+    setUser({
+      name,
+      email,
+    });
+
+    // Go to Dashboard
+    onNext();
+  };
+
   return (
     <div className="min-h-screen flex">
 
@@ -8,13 +27,14 @@ function LoginPage() {
         className="w-1/2 hidden md:block bg-cover bg-center"
         style={{
           backgroundImage:
-            "url('https://www.magnific.com/free-vector/chess-king-abstract-low-poly-design-strategy-concept-from-dot-line-vector-illustration_24126552.htm#fromView=keyword&page=1&position=8&uuid=b0e91d3b-f896-4df1-a8f9-7c4b090b8270&query=Chess+king')",
+            "url('https://images.unsplash.com/photo-1528819622765-d6bcf132f793?w=1200')",
         }}
       >
       </div>
 
       {/* Right Side - Login Form */}
       <div className="w-full md:w-1/2 bg-gray-900 flex justify-center items-center">
+
         <div className="bg-white p-8 rounded-xl shadow-2xl w-96">
 
           <h2 className="text-3xl font-bold text-center mb-2">
@@ -22,11 +42,28 @@ function LoginPage() {
           </h2>
 
           <p className="text-center text-gray-600 mb-6">
-            Welcome back , Tactician
+            Welcome back, Tactician
           </p>
 
-          <form>
+          <form onSubmit={handleSubmit}>
 
+            {/* Name */}
+            <div className="mb-4">
+              <label className="block mb-2 font-medium">
+                Name
+              </label>
+
+              <input
+                type="text"
+                placeholder="Enter your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="w-full border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                required
+              />
+            </div>
+
+            {/* Email */}
             <div className="mb-4">
               <label className="block mb-2 font-medium">
                 Email Address
@@ -35,10 +72,14 @@ function LoginPage() {
               <input
                 type="email"
                 placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="w-full border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                required
               />
             </div>
 
+            {/* Password */}
             <div className="mb-2">
               <label className="block mb-2 font-medium">
                 Password
@@ -47,7 +88,10 @@ function LoginPage() {
               <input
                 type="password"
                 placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className="w-full border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                required
               />
             </div>
 
@@ -70,6 +114,7 @@ function LoginPage() {
           </form>
 
           <div className="text-center mt-5">
+
             <p className="text-gray-600">
               Don't have an account?
             </p>
@@ -77,9 +122,11 @@ function LoginPage() {
             <button className="mt-2 text-blue-600 font-semibold hover:underline">
               Create New Account
             </button>
+
           </div>
 
         </div>
+
       </div>
 
     </div>
